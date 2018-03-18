930d91ea3c71e5d10c68e3d59dd38faa3eaaf342
Denis Nazarenko <den.nazarenko@icloud.com>
2018-03-18 20:01:15 +0300

    Defined routes and templates

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,10 +1,14 @@
 import Route from '@ember/routing/route';
-import EmberObject from '@ember/object';
+import EmberObject, { computed } from '@ember/object';
 
 import { A } from '@ember/array';
+import { dasherize } from '@ember/string';
 
 let Band = EmberObject.extend({
   name: '',
+  slug: computed('name', function() {
+    return dasherize(this.get('name'));
+  })
 });
 
 let Song = EmberObject.extend({
