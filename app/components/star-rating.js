52f0d959e1a9eedd89e6124689ca938bff6f1b90
Denis Nazarenko <den.nazarenko@icloud.com>
2018-03-26 20:41:04 +0300

    Star rating component

---
+++ app/components/star-rating.js
@@ -0,0 +1,18 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  classNames: ['rating-panel'],
+
+  rating: 0,
+  maxRating: 5,
+
+  stars: computed('rating', 'maxRating', function() {
+    let stars = [];
+    let rating = this.get('rating');
+    for (let i=1; i <= this.get('maxRating'); i++) {
+      stars.push({ rating: i, isFull: rating >= i });
+    }
+    return stars;
+  }),
+});
