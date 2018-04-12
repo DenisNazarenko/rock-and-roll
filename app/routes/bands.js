fdc72bfa88d6996796ab4cdf7c51a075b82f396a
Denis Nazarenko <den.nazarenko@icloud.com>
2018-04-12 08:26:58 +0300

    Added loading and error state

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,17 +1,7 @@
-import { Promise as EmberPromise } from 'rsvp';
 import Route from '@ember/routing/route';
 
-function wait(delay) {
-  return new EmberPromise(function(resolve) {
-    setTimeout(function() {
-      resolve();
-    }, delay);
-  });
-}
-
 export default Route.extend({
-  async model() {
-    await wait(3000);
+  model() {
     return this.store.findAll('band');
   },
 
---
+++ tests/unit/utils/wait-test.js
@@ -0,0 +1,11 @@
+import wait from 'rock-and-roll/utils/wait';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | wait', function(hooks) {
+
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = wait();
+    assert.ok(result);
+  });
+});
